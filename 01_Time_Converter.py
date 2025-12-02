9732b170ae5a056518bec2a4f23768e87b3a857e
Tyson Bermuda <alampurjprathish@gmail.com>
2025-12-02 21:46:09 +0530

    Delete 01_Time_Converter.py

--- 01_Time_Converter.py
+++
@@ -1,9 +0,0 @@
-# Time Converter
-# Convert total minutes into hours and minutes
-
-minutes = int(input("Enter minutes: "))
-
-hours = minutes // 60            # integer division
-remaining_minutes = minutes % 60 # modulo operator
-
-print(f"{hours} hrs {remaining_minutes} mins")
